8da710a2607c520811fe63b870d0135cdc53c183
Wendrew nick costa tatehira <niickcostta00@outlook.com>
2023-06-21 11:29:47 -0400

    Organizado Models

--- ProductsApi/Models/ComputerComponents.cs
+++ ProductsApi/Models/ComputerComponents.cs
@@ -1,38 +1,32 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.Globalization;
+using static ProductsApi.Models.Enums;
 
 namespace ProductsApi.Models
 {
     [Table("ComputerComponent")]
     public class ComputerComponents
     {
+        // Componentes
         public int Id { get; set; }
         public string Cpu { get; set; }
-        public int TdpCpu { get; set; }
         public string Gpu { get; set; }
-        public int TdpGpu { get; set; }
         public string Motherboard { get; set; }
-        public int TdpMotherboard { get; set; }
         public string Ram { get; set; }
-        public int TdpRam { get; set; }
         public int QntRam { get; set; }
-        public int TdpSSD { get; set; }
-        public string HDD { get; set; }
+
+        // TDPs
+        public int TdpCpu { get; set; }
+        public int TdpGpu { get; set; }
+        public int TdpRam { get; set; }
+        public int TdpMotherboard { get; set; }
         public int TdpHDD { get; set; }
+        public int TdpSSD { get; set; }
         public int TdpTotal { get; set; }
 
+        // Enums
+        public HDDType HDD { get; set; }
         public SSDType SSD { get; set; }
         public RotuloCategory RotuloCategory { get; set; }
-    }
-    public enum SSDType
-    {
-        Sata = 0,
-        Nvme = 1,
-    }
-
-    public enum RotuloCategory
-    {
-        Upgrade = 0,
-        Novo = 1,
     }
 }
