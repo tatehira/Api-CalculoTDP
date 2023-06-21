ece027eb93da372c9f50e871b51516b91a59af0a
Wendrew nick costa tatehira <niickcostta00@outlook.com>
2023-06-21 10:56:40 -0400

    Update ComponentData - Lista de processadores

---
+++ ProductsApi/Models/CompomentData.cs
@@ -0,0 +1,50 @@
+﻿namespace ProductsApi.Models
+{
+    public static class CompomentData
+    {
+        public static List<ComputerComponents> ProcessorList { get; set; } = new List<ComputerComponents>
+        {
+            new ComputerComponents { Cpu = "Intel Core i9-11900K", TdpCpu = 125 },
+            new ComputerComponents { Cpu = "Intel Core i7-11700K", TdpCpu = 125 },
+            new ComputerComponents { Cpu = "Intel Core i5-11600K", TdpCpu = 125 },
+            new ComputerComponents { Cpu = "Intel Core i9-10900K", TdpCpu = 125 },
+            new ComputerComponents { Cpu = "Intel Core i7-10700K", TdpCpu = 125 },
+            new ComputerComponents { Cpu = "AMD Ryzen 9 5950X", TdpCpu = 105 },
+            new ComputerComponents { Cpu = "AMD Ryzen 9 5900X", TdpCpu = 105 },
+            new ComputerComponents { Cpu = "AMD Ryzen 7 5800X", TdpCpu = 105 },
+            new ComputerComponents { Cpu = "AMD Ryzen 5 5600X", TdpCpu = 65 },
+            new ComputerComponents { Cpu = "Intel Core i5-10600K", TdpCpu = 125 },
+            new ComputerComponents { Cpu = "Intel Core i9-10850K", TdpCpu = 125 },
+            new ComputerComponents { Cpu = "AMD Ryzen 7 3700X", TdpCpu = 65 },
+            new ComputerComponents { Cpu = "AMD Ryzen 5 3600X", TdpCpu = 95 },
+            new ComputerComponents { Cpu = "Intel Core i5-10400F", TdpCpu = 65 },
+            new ComputerComponents { Cpu = "Intel Core i7-9700K", TdpCpu = 95 },
+            new ComputerComponents { Cpu = "Intel Core i9-12900K", TdpCpu = 125 },
+            new ComputerComponents { Cpu = "Intel Core i7-12700K", TdpCpu = 125 },
+            new ComputerComponents { Cpu = "Intel Core i5-12600K", TdpCpu = 125 },
+            new ComputerComponents { Cpu = "Intel Core i9-11900KF", TdpCpu = 125 },
+            new ComputerComponents { Cpu = "Intel Core i7-11700KF", TdpCpu = 125 },
+            new ComputerComponents { Cpu = "AMD Ryzen 9 5900HX", TdpCpu = 45 },
+            new ComputerComponents { Cpu = "AMD Ryzen 7 5800HX", TdpCpu = 45 },
+            new ComputerComponents { Cpu = "AMD Ryzen 5 5600H", TdpCpu = 45 },
+            new ComputerComponents { Cpu = "AMD Ryzen 9 5950H", TdpCpu = 45 },
+            new ComputerComponents { Cpu = "Intel Core i9-10900KF", TdpCpu = 125 },
+            new ComputerComponents { Cpu = "Intel Core i7-10700KF", TdpCpu = 125 },
+            new ComputerComponents { Cpu = "AMD Ryzen 7 3700XT", TdpCpu = 65 },
+            new ComputerComponents { Cpu = "AMD Ryzen 5 3600XT", TdpCpu = 95 },
+            new ComputerComponents { Cpu = "Intel Core i5-10400", TdpCpu = 65 },
+            new ComputerComponents { Cpu = "Intel Core i7-9700KF", TdpCpu = 95 },
+            new ComputerComponents { Cpu = "Xeon E3-1220 v3", TdpCpu = 80 },
+            new ComputerComponents { Cpu = "Xeon E5-2620 v4", TdpCpu = 85 },
+            new ComputerComponents { Cpu = "Xeon E5-1650 v3", TdpCpu = 140 },
+            new ComputerComponents { Cpu = "Xeon E5-2680 v4", TdpCpu = 120 },
+            new ComputerComponents { Cpu = "Xeon E5-2690 v3", TdpCpu = 135 },
+            new ComputerComponents { Cpu = "Xeon E3-1270 v5", TdpCpu = 80 },
+            new ComputerComponents { Cpu = "Xeon E3-1230 v3", TdpCpu = 80 },
+            new ComputerComponents { Cpu = "Xeon E5-2630 v4", TdpCpu = 85 },
+            new ComputerComponents { Cpu = "Xeon E5-1660 v3", TdpCpu = 140 },
+            new ComputerComponents { Cpu = "Xeon E5-2683 v4", TdpCpu = 120 }
+        };
+        
+    }
+}
